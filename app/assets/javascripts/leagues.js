76dd57b292fa999eae365b5e5ae3b52c93fd83a4
lordp <darryl@addict.net.nz>
2013-04-15 10:02:59 +1200

    Javascript code to calculate the split column

---
+++ app/assets/javascripts/leagues.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $('.split-target').each(function() {
+    var target_type = '';
+    if ($(this).hasClass('split-target-driver')) {
+      target_type = 'driver';
+    }
+    else {
+      target_type = 'team';
+    }
+
+    var index = this.id.replace(target_type + '-split-', '');
+    if (index > 1) {
+      var split_value = $('#' + target_type + '-points-' + (index - 1).toString()).html() - $('#' + target_type + '-points-' + index.toString()).html();
+      $(this).html(split_value);
+    }
+    else {
+      $(this).html('--');
+    }
+  });
+});
