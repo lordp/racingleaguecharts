319412c3fd65c1ab8cc08620f6e5dac7b9181972
lordp <darryl@addict.net.nz>
2013-04-14 23:51:40 +1200

    Set up base route

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ Virtualwdc::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'leagues#index'
 
   # See how all your routes lay out with "rake routes"
 
