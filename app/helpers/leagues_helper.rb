92264fa885bd9ee0570fb38db9d53280d457baf4
lordp <darryl@addict.net.nz>
2013-04-15 10:13:30 +1200

    Add a title to explain the points for each result

--- app/helpers/leagues_helper.rb
+++ app/helpers/leagues_helper.rb
@@ -2,6 +2,7 @@ module LeaguesHelper
 
   def driver_race_result(driver, race)
     styles = []
+    title = ''
 
     if result = driver.results.find_by_race_id(race)
       case result.position
@@ -10,15 +11,19 @@ module LeaguesHelper
         styles << 'background: rgb(239, 207, 255)'
       when 1 then
         value = result.position
+        title = pluralize(Driver::POINTS[result.position - 1], 'point')
         styles << 'background: rgb(255, 255, 191)'
       when 2 then
         value = result.position
+        title = pluralize(Driver::POINTS[result.position - 1], 'point')
         styles << 'background: rgb(223, 223, 223)'
       when 3 then
         value = result.position
+        title = pluralize(Driver::POINTS[result.position - 1], 'point')
         styles << 'background: rgb(255, 223, 159)'
       when 4..8 then
         value = result.position
+        title = pluralize(Driver::POINTS[result.position - 1], 'point')
         styles << 'background: rgb(223, 255, 223)'
       else
         value = result.position
@@ -28,7 +33,7 @@ module LeaguesHelper
       styles << 'font-style: italic' if result.fastest_lap?
       styles << 'font-weight: bold' if result.pole_position?
 
-      value = link_to(value, edit_admin_result_path(result))
+      value = link_to(value, edit_admin_result_path(result), :title => title)
     else
       value = link_to('-', new_admin_result_path(:race_id => race, :driver_id => driver))
     end
