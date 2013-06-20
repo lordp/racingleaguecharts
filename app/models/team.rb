5bebfdf15af3414bf4ba906d2b3bea0702c494cb
lordp <darryl@addict.net.nz>
2013-06-20 14:26:15 +1200

    Add option to assign teams to leagues

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,10 +1,11 @@
 class Team < ActiveRecord::Base
-  attr_accessible :name, :flair, :country
+  attr_accessible :name, :flair, :country, :league_ids
 
   has_many :drivers
   has_many :results
 
   has_many :league_teams
+  has_many :leagues, :through => :league_teams
 
   def points(league)
     league_teams.where(:league_id => league).first.points
