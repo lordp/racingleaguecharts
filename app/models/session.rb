14498f76d2e3291a83f2cc7d78538a9413bd0e6d
lordp <darryl@addict.net.nz>
2013-12-02 14:44:59 +1300

    Add method to calculate the average lap for a session

--- app/models/session.rb
+++ app/models/session.rb
@@ -4,4 +4,8 @@ class Session < ActiveRecord::Base
   has_many :laps
   has_many :races, :through => :laps, :uniq => true
   has_many :drivers, :through => :laps, :uniq => true
+
+  def average_lap
+    laps.average(:total)
+  end
 end
