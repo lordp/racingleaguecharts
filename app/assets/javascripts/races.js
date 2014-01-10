a7621b8066c3f7ac4a001f19aa21b2bfeba344e2
Darryl Hamilton <darryl@addict.net.nz>
2014-01-10 17:50:56 +1300

    Remove unused code

--- app/assets/javascripts/races.js
+++ app/assets/javascripts/races.js
@@ -1,10 +1,3 @@
 $(function() {
-  $('#race_session_list').on('dblclick', function(e) {
-    console.log(e.target);
-    $('#session_ids').append($(e.target));
-  });
 
-  $('#session_ids').on('dblclick', function(e) {
-    $('#race_session_list').append($(e.target));
-  });
 });
--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -14,11 +14,7 @@ class RacesController < ApplicationController
   end
 
   def create
-    params[:race].delete(:session_list)
     @race = Race.new(params[:race])
-    params[:session_ids].each do |id|
-      @race.sessions_attributes << { :id => id }
-    end
     if @race.save
       redirect_to(races_path)
     end
@@ -28,8 +24,6 @@ class RacesController < ApplicationController
   end
 
   def update
-    params[:race].delete(:session_list)
-    @race.session_ids = params[:session_ids]
     if @race.update_attributes(params[:race])
       redirect_to(races_path)
     end
