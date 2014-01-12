fc78a8da7cea89052cb6a093fbed5179b0dd4751
lordp <darryl@addict.net.nz>
2014-01-13 09:08:02 +1300

    If the blank option is chosen in the track selector, show all sessions

--- app/assets/javascripts/races.js
+++ app/assets/javascripts/races.js
@@ -2,7 +2,16 @@ $(function() {
   $('#race_track_id').on('change', function(e) {
     var selected_track = $(e.target[e.target.selectedIndex]).val();
     $('#race_session_ids').empty();
-    $.each(sessions[selected_track], function(i, track) {
+    if (sessions.hasOwnProperty(selected_track)) {
+      var track_sessions = sessions[selected_track];
+    }
+    else {
+      var track_sessions = [];
+      for (var track in sessions) {
+        track_sessions = track_sessions.concat(sessions[track])
+      }
+    }
+    $.each(track_sessions, function(i, track) {
       $('<option/>').attr('value', track.id).html(track.name).appendTo($('#race_session_ids'));
     });
   });
