7826330a7f4d223dce9805688c8738a68eda4a9d
lordp <darryl@addict.net.nz>
2014-01-13 15:57:18 +1300

    Fix bug with setting track id

--- db/migrate/20140111001606_set_track_id_on_races.rb
+++ db/migrate/20140111001606_set_track_id_on_races.rb
@@ -1,7 +1,7 @@
 class SetTrackIdOnRaces < ActiveRecord::Migration
   def up
     Race.all.each do |race|
-      race.update_attribute(:track_id, race.sessions.first.track_id)
+      race.update_attribute(:track_id, race.sessions.first.try(:track_id))
     end
   end
 end
