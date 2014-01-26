b04c626e38d7056dfdd88ccd28a0dff106ac7b7a
Darryl Hamilton <darryl@addict.net.nz>
2014-01-26 17:13:40 +1300

    Improve the flexibility of the menu builder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  def build_menu(super_league = nil, league = nil, season = nil, race = nil)
-    @super_leagues = SuperLeague.all
-    @leagues = super_league.leagues if super_league
-    @seasons = league.seasons if league
-    @races = season.races if season
+  def build_menu(obj = nil)
+    case obj
+      when Session
+        @race = obj.race
+        build_menu(@race)
+      when Race
+        @season = obj.season
+        @races = @season.races
+        build_menu(@season)
+      when Season
+        @league = obj.league
+        @seasons = @league.seasons
+        @races = @season.races
+        build_menu(@league)
+      when League
+        @super_league = obj.super_league
+        @leagues = @super_league.leagues
+        @seasons = obj.seasons
+        build_menu(@super_league)
+      when SuperLeague
+        @super_leagues = SuperLeague.all
+        @leagues = @super_league.leagues
+      else
+        @super_leagues = SuperLeague.all
+    end
   end
 
 end
