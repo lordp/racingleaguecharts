8fec36892c844cc22e2fa4860a9a5f94c39d59d0
Darryl Hamilton <darryl@addict.net.nz>
2014-01-26 17:12:54 +1300

    Expand routes

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,14 @@ Virtualwdc::Application.routes.draw do
     end
   end
 
+  resources :leagues do
+    resources :seasons
+  end
+
+  resources :seasons do
+    resources :races
+  end
+
   resources :races do
     member do
       get 'chart'
