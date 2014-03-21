10886b88ed40ec0e8ba8006e9715dba80f66b944
Darryl Hamilton <darryl@addict.net.nz>
2014-03-21 19:09:32 +1300

    Move driver editing to admin section

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,44 +1,12 @@
 class DriversController < ApplicationController
 
-  before_filter :find_driver, :except => [ :index, :create, :new ]
-
   def index
-    @drivers = Driver.all
+    @drivers = Driver.order(:name)
   end
 
   def show
+    @driver = Driver.find(params[:id].to_i)
     @sessions = @driver.sessions.order(:token).page(params[:page].to_i).per(15)
   end
-
-  def claim
-    @driver.update_attribute(:name, params[:driver][:name])
-    redirect_to(drivers_path)
-  end
-
-  def new
-    @driver = Driver.new
-  end
-
-  def create
-    @driver = Driver.new(params[:driver])
-    if @driver.save
-      redirect_to(driver_path(@driver), :notice => "Driver created")
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if @driver.update_attributes(params[:driver])
-      redirect_to(driver_path(@driver), :notice => "Driver updated")
-    end
-  end
-
-  private
-
-    def find_driver
-      @driver = Driver.find(params[:id].to_i) if params[:id]
-    end
 
 end
---
+++ app/controllers/say_what/drivers_controller.rb
@@ -0,0 +1,47 @@
+class SayWhat::DriversController < ApplicationController
+  before_filter :find_driver, :only => [ :show, :edit, :update, :destroy ]
+
+  def index
+    @drivers = Driver.order(:name)
+  end
+
+  def show
+    @sessions = @driver.sessions.order(:created_at).page(params[:page].to_i).per(15)
+  end
+
+  def new
+    @driver = Driver.new
+  end
+
+  def edit
+  end
+
+  def create
+    @driver = Driver.new(params[:driver])
+
+    if @driver.save
+      redirect_to(say_what_driver_path(@driver), :notice => 'Driver was successfully created.')
+    else
+      render "new"
+    end
+  end
+
+  def update
+    if @driver.update_attributes(params[:driver])
+      redirect_to(say_what_driver_path(@driver), :notice => 'Driver was successfully updated.')
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @driver.destroy
+    redirect_to(say_what_drivers_url, :notice => 'Driver deleted')
+  end
+
+  private
+
+    def find_driver
+      @driver = Driver.find(params[:id].to_i) if params[:id]
+    end
+end
