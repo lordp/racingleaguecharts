70bab1feb9ac7961d86e6865335dac80d6f3dc7f
Darryl Hamilton <darryl@addict.net.nz>
2014-03-23 08:37:44 +1300

    Reinstate screenshot editing for help with time verification

--- app/controllers/screenshots_controller.rb
+++ app/controllers/screenshots_controller.rb
@@ -1,11 +1,22 @@
 class ScreenshotsController < ApplicationController
-  before_filter :find_screenshot, :only => [ :show ]
+  before_filter :find_screenshot, :only => [ :show, :edit, :update ]
   before_filter :menu
 
   def index
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @screenshot.update_attributes(params[:screenshot])
+      redirect_to(session_path(@screenshot.session), :notice => 'Screenshot was successfully updated.')
+    else
+      render "edit"
+    end
   end
 
   private
