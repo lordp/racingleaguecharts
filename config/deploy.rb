697e7a45205ef015cbfafd263b79ccc75b31fcb8
lordp <darryl@addict.net.nz>
2014-03-25 10:15:58 +1300

    Add reddit config to linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :format, :pretty
 set :log_level, :info
 # set :pty, true
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/reddit.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
