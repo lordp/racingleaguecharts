468b23a4e21ff8cf1d182a3f021a38df117183de
lordp <darryl@addict.net.nz>
2014-04-01 15:39:37 +1300

    Don't hide the short session info when expanding the panel

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,9 +1,7 @@
 $(function() {
   $('.session-collapsible').on('hide.bs.collapse', function () {
-    $('#short-' + this.id).show();
     $('#link-' + this.id).removeClass('glyphicon-minus').addClass('glyphicon-plus');
   }).on('show.bs.collapse', function() {
-    $('#short-' + this.id).hide();
     $('#link-' + this.id).removeClass('glyphicon-plus').addClass('glyphicon-minus');
   });
 
