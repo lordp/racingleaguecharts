268a30e934fa477da3561b27f8ed406306b8fd5b
Darryl Hamilton <darryl@addict.net.nz>
2014-05-03 23:14:49 +1200

    Add gap to winner to gap column

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -140,7 +140,7 @@ module ApplicationHelper
         "+#{pluralize((sessions[0].laps.count - session.laps.count).abs, 'lap')}"
       else
         previous_session = sessions[index - 1]
-        "+#{nice_time((previous_session.total_time - session.total_time).abs)}"
+        "+#{nice_time((previous_session.total_time - session.total_time).abs)} / +#{nice_time((sessions[0].total_time - session.total_time).abs)}"
       end
     end
   end
