ac40564b229e528e4c7c225d775c8d3c80e5ec65
Darryl Hamilton <darryl@addict.net.nz>
2014-05-03 15:47:42 +1200

    Stop unauthorized editing of someone elses session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,8 @@ class SessionsController < ApplicationController
 
   before_filter :find_session, :only => [ :show, :chart, :edit, :update ]
   skip_before_filter :build_menu, :only => [ :register ]
-
   skip_before_filter :verify_authenticity_token, :only => [ :register ]
+  before_filter :authorize_claimed, :only => [ :edit, :update ]
 
   def index
     @sessions = Session.order(:created_at).page(params[:page].to_i).per(15)
@@ -43,6 +43,10 @@ class SessionsController < ApplicationController
 
     def find_session
       @session = Session.find(params[:id].to_i)
+    end
+
+    def authorize_claimed
+      redirect_to(user_path(current_user), :alert => 'Not authorized!') if current_user.nil? || !current_user.has_claimed?(@session.driver)
     end
 
 end
