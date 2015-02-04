ca2aa6899466e24ecf7861cea93a53709cb99eba
Darryl Hamilton <darryl@addict.net.nz>
2015-02-05 08:38:08 +1300

    Remove secret_token file and update app name config

--- Rakefile
+++ Rakefile
@@ -4,4 +4,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-Virtualwdc::Application.load_tasks
+RacingLeagueCharts::Application.load_tasks
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Virtualwdc::Application.config.secret_token = 'b4509337e15c4b83ae9c6ada40d2ad3a8d3433069a5b2649874e7b63aada468d489ded4e0d56814b56b56346a5bdf9c207c58afa0b517fe10bdf5f706ea3fecf'
