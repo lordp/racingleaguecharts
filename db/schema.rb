159464e20fa515254fd0c6b13b79c8b3508bee7c
Darryl Hamilton <darryl@addict.net.nz>
2015-04-04 11:57:06 +1300

    Update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,33 +9,39 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140910074714) do
+ActiveRecord::Schema.define(version: 20150326025822) do
 
-  create_table "driver_aliases", :force => true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "driver_aliases", force: :cascade do |t|
     t.integer  "driver_id"
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
-  create_table "driver_users", :force => true do |t|
+  create_table "driver_users", force: :cascade do |t|
     t.integer  "driver_id"
     t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "drivers", :force => true do |t|
-    t.string "name",   :limit => 50
-    t.string "ip",     :limit => 50
-    t.string "flair"
-    t.string "colour"
-    t.string "marker"
+  add_index "driver_users", ["driver_id", "user_id"], name: "driver_users_driver_id_user_id", using: :btree
+
+  create_table "drivers", force: :cascade do |t|
+    t.string  "name",           limit: 50
+    t.string  "ip",             limit: 50
+    t.string  "flair",          limit: 255
+    t.string  "colour",         limit: 255
+    t.string  "marker",         limit: 255
+    t.integer "sessions_count"
   end
 
-  create_table "laps", :force => true do |t|
+  create_table "laps", force: :cascade do |t|
     t.integer "race_id"
     t.integer "driver_id"
     t.integer "lap_number"
@@ -44,61 +50,62 @@ ActiveRecord::Schema.define(:version => 20140910074714) do
     t.float   "sector_3"
     t.float   "total"
     t.integer "session_id"
-    t.string  "thing"
+    t.string  "thing",      limit: 255
     t.decimal "speed"
     t.integer "position"
     t.decimal "fuel"
   end
 
-  create_table "league_seasons", :force => true do |t|
+  create_table "league_seasons", force: :cascade do |t|
     t.integer  "league_id"
     t.integer  "season_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "leagues", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+  create_table "leagues", force: :cascade do |t|
+    t.string   "name",            limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.integer  "super_league_id"
   end
 
-  create_table "races", :force => true do |t|
-    t.string   "name",       :limit => 50
+  create_table "races", force: :cascade do |t|
+    t.string   "name",       limit: 50
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "season_id"
     t.integer  "track_id"
     t.boolean  "time_trial"
     t.boolean  "is_dry"
-    t.string   "thing"
+    t.string   "thing",      limit: 255
     t.boolean  "fia"
   end
 
-  create_table "screenshots", :force => true do |t|
+  create_table "screenshots", force: :cascade do |t|
     t.integer  "session_id"
     t.text     "parsed"
-    t.string   "image"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
+    t.string   "image",              limit: 255
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.boolean  "confirmed"
+    t.boolean  "career"
   end
 
-  create_table "seasons", :force => true do |t|
-    t.string   "name"
+  create_table "seasons", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.integer  "league_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.boolean  "time_trial"
   end
 
-  create_table "sessions", :force => true do |t|
-    t.string   "token",         :limit => 50
+  create_table "sessions", force: :cascade do |t|
+    t.string   "token",             limit: 50
     t.float    "session_type"
     t.integer  "position"
     t.integer  "driver_id"
@@ -106,33 +113,41 @@ ActiveRecord::Schema.define(:version => 20140910074714) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "race_id"
-    t.boolean  "winner",                      :default => false
+    t.boolean  "winner",                       default: false
     t.boolean  "is_dry"
     t.text     "lap_text"
     t.integer  "grid_position"
+    t.integer  "laps_count"
+    t.integer  "screenshots_count"
+    t.integer  "fastest_lap_id"
   end
 
-  create_table "super_leagues", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  add_index "sessions", ["driver_id"], name: "sessions_driver_id", using: :btree
+
+  create_table "super_leagues", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.boolean  "disabled"
   end
 
-  create_table "tracks", :force => true do |t|
-    t.string   "name"
+  add_index "super_leagues", ["disabled"], name: "idx_disabled", using: :btree
+
+  create_table "tracks", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.float    "length"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+  create_table "users", force: :cascade do |t|
+    t.string   "email",           limit: 255
+    t.string   "password_digest", limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.boolean  "admin"
-    t.string   "token"
-    t.string   "name"
+    t.string   "token",           limit: 255
+    t.string   "name",            limit: 255
   end
 
 end
