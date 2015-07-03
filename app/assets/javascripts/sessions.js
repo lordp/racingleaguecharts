c56a6d60e11d7b052979b3b2a12fa81dd95174b1
Darryl Hamilton <darryl@addict.net.nz>
2015-07-03 20:07:32 +1200

    Move session comparison js to separate file

---
+++ app/assets/javascripts/sessions.js
@@ -0,0 +1,17 @@
+$(function() {
+  $('.session-compare').on('click', function() {
+    params = {
+      drivers: $('#driver').val() || [],
+      tracks: $('#track').val() || [],
+      session_type: $.map($('input.session-compare:checked'), function(item) { return item.id } ) || []
+    };
+
+    if (params.drivers.length > 0 || params.tracks.length > 0 || params.session_type.length > 0) {
+      $.get('/sessions/search.json?' + $.param(params), function (data) {
+        $('#session_list tbody').empty().append($.map(data, function(item) {
+          return "<tr><td><a href=\"?compare=" + item.id + "\">" + item.driver + "</a></td><td>" + item.track + "</td><td>" + item.session_type + "</td><td>" + item.race + "</td><td>" + item.laps + "</td><td>" + item.created_at + "</td></tr>";
+        }).join(''));
+      });
+    }
+  });
+});
